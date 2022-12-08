6cb8cb71d3c9df848b86812b8d5f076b0e786e70
Rubens Galarraga <rgalarraga121@gmail.com>
2022-12-08 18:35:56 -0300

    edit

---
+++ src/data/SkillsData.js
@@ -0,0 +1,119 @@
+import {
+	FaBootstrap,
+	FaCss3Alt,
+	FaGithub,
+	FaHtml5,
+	FaNpm,
+	FaReact,
+	FaSass,
+	FaYarn,
+	FaFigma,
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import { IoLogoNodejs } from "react-icons/io";
+import { RiVuejsFill } from "react-icons/ri";
+import {
+	SiAdobephotoshop,
+	SiJquery,
+	SiTailwindcss,
+	SiNextdotjs,
+	SiTypescript,
+	SiFirebase,
+	SiStyledcomponents,
+	SiFramer,
+	SiAdobeillustrator
+} from "react-icons/si";
+
+export const SkillsData = [
+	{
+		name: "HTML",
+		icon: <FaHtml5 />,
+	},
+	{
+		name: "CSS",
+		icon: <FaCss3Alt />,
+	},
+	{
+		name: "Javascript",
+		icon: <DiJavascript1 />,
+	},
+	{
+		name: "TypeScript",
+		icon: <SiTypescript />,
+	},
+	{
+		name: "jQuery",
+		icon: <SiJquery />,
+	},
+	{
+		name: "React Js",
+		icon: <FaReact />,
+	},
+	{
+		name: "Next Js",
+		icon: <SiNextdotjs />,
+	},
+	{
+		name: "Firebase",
+		icon: <SiFirebase />,
+	},
+	{
+		name: "JSON",
+		icon: <VscJson />,
+	},
+	{
+		name: "Bootstrap",
+		icon: <FaBootstrap />,
+	},
+	{
+		name: "Tailwind",
+		icon: <SiTailwindcss />,
+	},
+	{
+		name: "Sass",
+		icon: <FaSass />,
+	},
+	{
+		name: "Styled Components",
+		icon: <SiStyledcomponents />,
+	},
+	{
+		name: "Framer Motion",
+		icon: <SiFramer />,
+	},
+	{
+		name: "Git",
+		icon: <FaGithub />,
+	},
+	{
+		name: "Npm",
+		icon: <FaNpm />,
+	},
+	{
+		name: "Yarn",
+		icon: <FaYarn />,
+	},
+	{
+		name: "Figma",
+		icon: <FaFigma />,
+	},
+	{
+		name: "Photoshop",
+		icon: <SiAdobephotoshop />,
+	},
+	{
+		name: "Illustrator",
+		icon: <SiAdobeillustrator />,
+	},
+	{
+		name: "Nodejs",
+		icon: <IoLogoNodejs />,
+	},	
+	{
+		name: "Vue js",
+		icon: <RiVuejsFill />,
+	},
+];
+
+
---
+++ src/data/WorkData.js
@@ -0,0 +1,32 @@
+export const WorkData = [
+	{
+		title: "App e-commerce",
+		desc: "A commercial store referring to sporting goods.",
+		tech: ["React.js", "Firebase", "Css", 'Axios'],
+		gitlink: "//github.com/rubyg21/market-run-Galarraga",
+		site: "//ecomerce-d548b.web.app/",
+	},
+	{
+		title: "App Pokemon",
+		desc: "It is a web in which you can interact and guess what pokemon is",
+		tech: ["Vue.js", "Firebase", 'APIs', 'Axios'],
+		gitlink: "//github.com/rubyg21/pokemon-game",
+		site: "//vue3-pokemons.netlify.app/",
+	},
+	{
+		title: "App Movies ",
+		desc: "Here you can search for information on your favorite movie",
+		tech: ["React.js", "Css", "APIs", 'Axios'],
+		gitlink: "//github.com/rubyg21/movie-react",
+		site: "//movie-react2022.netlify.app/",
+	},
+	{
+		title: "App Pokedex ",
+		desc: "You can check information of your favorite pokemon",
+		tech: ["React.js", "Css", 'Axios', "APIs"],
+		gitlink: "//github.com/rubyg21/Pokedex-React",
+		site: "//pokedex-react2023.netlify.app/",
+	}
+
+
+];
